5cb8811662529ae09b8fbd594056e2623ee56df7
Parker Smith <parker@parkersmith.us>
2018-04-10 22:00:18 -0600

    Implement insertion sort

--- src/Sorting/Insertion.java
+++ src/Sorting/Insertion.java
@@ -1,12 +1,30 @@
 package Sorting;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 /**
  * Created by parker on 4/10/18.
  */
 public class Insertion extends Sort{
 
-    public void sort(Comparable[] arr)
+    public void sort(Comparable[] a)
     {
-
+        for (int i = 1; i < a.length; i++)
+        {
+            for (int j = i; j >= 1; j--)
+            {
+                if (less(a[j], a[j - 1]))
+                {
+                    exchange(a, j, j - 1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
     }
 }
