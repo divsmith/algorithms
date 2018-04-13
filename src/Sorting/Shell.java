5c82a68ea4c74113b3f11ac78efd510dddffe1e3
Parker Smith <parker@parkersmith.us>
2018-04-12 20:20:54 -0600

    Start shellsort with insertion sort

--- src/Sorting/Shell.java
+++ src/Sorting/Shell.java
@@ -5,8 +5,21 @@ package Sorting;
  */
 public class Shell extends Sort {
 
-    public void sort(Comparable[] arr)
+    public void sort(Comparable[] a)
     {
-
+        for (int i = 1; i < a.length; i++)
+        {
+            for (int j = i; j >= 1; j--)
+            {
+                if (less(a[j], a[j - 1]))
+                {
+                    exchange(a, j, j - 1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
     }
 }
