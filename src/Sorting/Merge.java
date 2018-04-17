31c0442492d80462605cf7e34ec55a0b8eada34c
Parker Smith <parker@parkersmith.us>
2018-04-16 19:57:03 -0600

    Implement issorted method

--- src/Sorting/Merge.java
+++ src/Sorting/Merge.java
@@ -3,5 +3,23 @@ package Sorting;
 /**
  * Created by parker on 4/15/18.
  */
-public class Merge {
+public class Merge extends Sort {
+
+    public void sort(Comparable[] arr)
+    {
+
+    }
+
+    private Boolean isSorted(Comparable[] arr)
+    {
+        for (int i = 1; i < arr.length; i++)
+        {
+            if (less(arr[i], arr[i - 1]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
