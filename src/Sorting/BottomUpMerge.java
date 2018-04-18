fd66e0fef278c00690a2a3785f015c3b6b4b9e91
Parker Smith <parker@parkersmith.us>
2018-04-17 22:59:29 -0600

    Add test for bottom up merge

---
+++ src/Sorting/BottomUpMerge.java
@@ -0,0 +1,12 @@
+package Sorting;
+
+/**
+ * Created by parker on 4/17/18.
+ */
+public class BottomUpMerge extends Sort {
+
+    public void sort(Comparable[] arr)
+    {
+
+    }
+}
---
+++ test/Sorting/BottomUpMergeTest.java
@@ -0,0 +1,26 @@
+package Sorting;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by parker on 4/17/18.
+ */
+class BottomUpMergeTest {
+
+    @Test
+    void test_sort_works()
+    {
+        Integer[] arr = {7, 9, 16, 15, 14, 8, 2, 10, 1, 13, 3, 11, 12, 5, 4, 6};
+        Integer[] sorted = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+
+        BottomUpMerge merge = new BottomUpMerge();
+        merge.sort(arr);
+
+        Assertions.assertTrue(Arrays.equals(arr, sorted));
+    }
+}
