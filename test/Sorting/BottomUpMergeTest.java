dc4534a2b4aa183ef3c16abceae2fb570dd1bea8
Parker Smith <parker@parkersmith.us>
2018-04-18 00:20:49 -0600

    Add test to force index out of bounds error

--- test/Sorting/BottomUpMergeTest.java
+++ test/Sorting/BottomUpMergeTest.java
@@ -61,6 +61,18 @@ class BottomUpMergeTest {
     }
 
     @Test
+    void test_sort_works_15_elements()
+    {
+        Integer[] arr = {7, 9, 16, 15, 14, 8, 2, 10, 1, 13, 3, 12, 5, 4, 6};
+        Integer[] sorted = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16};
+
+        BottomUpMerge merge = new BottomUpMerge();
+        merge.sort(arr);
+
+        Assertions.assertTrue(Arrays.equals(arr, sorted));
+    }
+
+    @Test
     void test_sort_works()
     {
         Integer[] arr = {7, 9, 16, 15, 14, 8, 2, 10, 1, 13, 3, 11, 12, 5, 4, 6};
