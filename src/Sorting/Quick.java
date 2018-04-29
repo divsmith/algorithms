7d40a961c7295223268af37edd341ea267ef64c9
Parker Smith <parker@parkersmith.us>
2018-04-29 15:22:48 -0600

    Create quicksort test

---
+++ src/Sorting/Quick.java
@@ -0,0 +1,14 @@
+package Sorting;
+
+import java.util.Comparator;
+
+/**
+ * Created by parker on 4/29/18.
+ */
+public class Quick extends Sort {
+
+    @Override
+    public void sort(Object[] arr, Comparator comparator) {
+
+    }
+}
